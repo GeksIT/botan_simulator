dc069c935776e0d89b06bc9e35645446459a5aa4
kazuare <kazuare@mail.ru>
2015-02-21 03:55:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -26,8 +26,8 @@
     
     
     */
-    
-    /*
+
+/*
     function Box(color) // Constructor
     {
         this.color = color;
@@ -41,93 +41,124 @@
     var blueBox = new Box("blue");
     alert(blueBox.getColor()); // will alert blue
     */
-    
-    function Upgrade(name,level,price,parameter,value){
-     this.name=name;
-     this.level=level;//уровень апгрейда
-     this.getPrice=price;//метод-возвращает цену в знаниях (на вход принимает следующий уровень)
-     this.parameter=parameter;//тот параметр,который увеличивает апгрейд
-     this.getValue=value;//метод- то, на сколько увеличивает
-     this.currentPrice=getPrice(1);
-     this.currentValue=getValue(1);
-     
+
+function Upgrade(name, price, parameter, value) {
+    this.name = name;
+    this.level = 0; //уровень апгрейда
+    this.getPrice = price; //метод-возвращает цену в знаниях (на вход принимает следующий уровень)
+    this.parameter = parameter; //тот параметр,который увеличивает апгрейд
+    this.getValue = value; //метод- то, на сколько увеличивает
+    this.currentPrice = price(0 + 1);
+    this.currentValue = value(0 + 1);
+
+}
+
+function getUpgradeByName(name){
+    for(var i=0;i<Player.upgrades.length;i++)
+        if(Player.upgrades[i].name==name)
+            return Player.upgrades[i];
+    return -1;
+}
+
+function activateUpgrade(name) {
+    var Up=getUpgradeByName(name);
+    Player[Up.parameter] += Up.currentValue;
+    Player["knowledgeToSpend"] -= Up.currentPrice;
+    Up.level++;
+    Up.currentPrice = Up.getPrice(Up.level + 1);
+    Up.currentValue = Up.getValue(Up.level + 1);
+}
+
+
+//присваивание стартовых переменнных
+
+UI = {};
+Player = {};
+Player.knowledge = 0;
+Player.totalKnowledge = 0;
+Player.knowledgeToSpend = 0;
+Player.speedOfStuding = 0;
+
+Player.speedInfimum = 0;
+Player.speedSupremum = 150;
+
+Player.hitPower = 1;
+Player.decreaseSpeed = 1;
+Player.upgrades = [];
+Player.upgrades[0] = new Upgrade("Autopunch", function (level) {return level*level*1000;}, "speedInfimum", function (level) {
+    return level*10;
+});
+
+
+//создание холста
+UI.stage = new createjs.Stage("canvas");
+
+//создание кнопки удара
+//обработка клика
+var hitFunction = function () {
+    //увеличение скорости ботания и обновление содержимого дивов
+    var x = Player.speedOfStuding + Player.hitPower;
+    if (x <= Player.speedSupremum) {
+        Player.speedOfStuding = x;
+    } else if (x <= Player.speedSupremum + Player.hitPower) {
+        Player.speedOfStuding = Player.speedSupremum;
     }
-    
-    //присваивание стартовых переменнных
-
-    UI={};
-    Player={};
-    Player.knowledge=0;
-    Player.totalKnowledge=0;
-    Player.knowledgeToSpend=0;
-    Player.speedOfStuding=0;
-    Player.speedLimit=150;
-    Player.Upgrades={};
-    Player.hitPower=1;
-    Player.decreaseSpeed=1;
-      
-   
-    //создание холста
-    UI.stage = new createjs.Stage("canvas");
-
-    //создание кнопки удара
-    //обработка клика
-     var hitFunction=function(){
-        //увеличение скорости ботания и обновление содержимого дивов
-        var x=Player.speedOfStuding+Player.hitPower;
-        if(x<=Player.speedLimit){
-           Player.speedOfStuding=x;
-        }else if(x<=Player.speedLimit+Player.hitPower){
-           Player.speedOfStuding=Player.speedLimit;
-        }
-        UI.speedCounter.text="Speed of studing: "+Player.speedOfStuding;
-        UI.stage.update();
-        
-    }
-
-    //создание кнопки удара
-    UI.hitButton = new createjs.Shape();
-    UI.hitButton.graphics.beginFill("DeepSkyBlue").drawRect(200,100,400,300);
-    UI.hitButton.addEventListener("click", hitFunction);
-    UI.stage.addChild(UI.hitButton);
-    
-
-    //создание счетчика знаний
-    UI.totalKnowledgeCounter=new createjs.Text("Your total knowledge: "+Player.totalKnowledge,"40px Arial","black");
-    UI.totalKnowledgeCounter.x=200;
-    UI.totalKnowledgeCounter.y=450;
-    UI.stage.addChild(UI.totalKnowledgeCounter);
-
-    //создание счетчика знаний, которых можно тратить
-    UI.knowledgeToSpendCounter=new createjs.Text("Your knowledge to spend: "+Player.knowledgeToSpend,"40px Arial","black");
-    UI.knowledgeToSpendCounter.x=200;
-    UI.knowledgeToSpendCounter.y=500;
-    UI.stage.addChild(UI.knowledgeToSpendCounter);
-    
-    //создание счетчика скорости ботания
-    UI.speedCounter=new createjs.Text("Speed of studing: "+Player.speedOfStuding,"40px Arial","black");
-    UI.speedCounter.x=200;
-    UI.speedCounter.y=550;
-    UI.stage.addChild(UI.speedCounter);
-
-    //обновление картинки
+    UI.speedCounter.text = "Learning rate: " + Player.speedOfStuding * 2 + " per second";
     UI.stage.update();
 
-    //тикер - каждые 0,6 сек 
-    setInterval(function(){
-        //  увеличение знаний
-        Player.totalKnowledge+=Player.speedOfStuding;
-        Player.knowledgeToSpend+=Player.speedOfStuding;
-        //  уменьшение скорости
-        if(Player.speedOfStuding>=1)
-            Player.speedOfStuding-=Player.decreaseSpeed;
-        //обновление дивов
-        UI.speedCounter.text="Speed of studing: "+Player.speedOfStuding;
-        UI.totalKnowledgeCounter.text="Your total knowledge: "+Player.totalKnowledge;
-        UI.knowledgeToSpendCounter.text="Your knowledge to spend: "+Player.knowledgeToSpend;
-        UI.stage.update();
-    },500);
-    
+};
+
+//создание кнопки удара
+UI.hitButton = new createjs.Shape();
+UI.hitButton.graphics.beginFill("DeepSkyBlue").drawRect(200, 100, 400, 300);
+UI.hitButton.addEventListener("click", hitFunction);
+UI.stage.addChild(UI.hitButton);
+
+
+//создание счетчика знаний
+UI.totalKnowledgeCounter = new createjs.Text("Total knowledge: " + Player.totalKnowledge, "30px Serif", "black");
+UI.totalKnowledgeCounter.x = 20;
+UI.totalKnowledgeCounter.y = 550;
+UI.stage.addChild(UI.totalKnowledgeCounter);
+
+//создание счетчика знаний, которых можно тратить
+UI.knowledgeToSpendCounter = new createjs.Text("Knowledge to spend: " + Player.knowledgeToSpend, "30px Serif", "black");
+UI.knowledgeToSpendCounter.x = 20;
+UI.knowledgeToSpendCounter.y = 510;
+UI.stage.addChild(UI.knowledgeToSpendCounter);
+
+//создание счетчика скорости ботания
+UI.speedCounter = new createjs.Text("Learning rate: " + Player.speedOfStuding * 2 + " per second", "30px Serif", "black");
+UI.speedCounter.x = 20;
+UI.speedCounter.y = 5;
+UI.stage.addChild(UI.speedCounter);
+
+UI.upgradeButtons=[];
+UI.upgradeButtons[0]
+//апгрейд autopunch
+UI.upgradeButtons[0] = new createjs.Shape();
+UI.upgradeButtons[0].graphics.beginFill("Magenta").drawRect(650,150 , 50, 50);
+UI.upgradeButtons[0].addEventListener("click", function(){activateUpgrade("Autopunch");});
+UI.stage.addChild(UI.upgradeButtons[0]);
+
+//обновление картинки
+UI.stage.update();
+
+//тикер - каждые 0,6 сек 
+setInterval(function () {
+    //  увеличение знаний
+    Player.totalKnowledge += Player.speedOfStuding;
+    Player.knowledgeToSpend += Player.speedOfStuding;
+    //  уменьшение скорости
+    if (Player.speedOfStuding >= Player.speedInfimum + Player.decreaseSpeed) Player.speedOfStuding -= Player.decreaseSpeed;
+    //обновление дивов
+    UI.speedCounter.text = "Learning rate: " + Player.speedOfStuding * 2 + " per second";
+    UI.totalKnowledgeCounter.text = "Total knowledge: " + Player.totalKnowledge;
+    UI.knowledgeToSpendCounter.text = "Knowledge to spend: " + Player.knowledgeToSpend;
+    UI.stage.update();
+}, 500);
+
+
 
 // работаем в jsfiddle.net
 //не забыть подключить последнюю версию jquery в менюшке слева
